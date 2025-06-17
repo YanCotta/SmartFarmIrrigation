a91fff1915e2a017649cbb99e2e0f43a72e2ad2f
Yan Cotta <yanpcotta@gmail.com>
2025-06-17 18:36:10 -0300

    fix(verify_db): update database connection path to relative reference

--- scripts/verify_db.py
+++ scripts/verify_db.py
@@ -1,5 +1,5 @@
 import sqlite3
-conn = sqlite3.connect('irrigation.db')
+conn = sqlite3.connect('../irrigation.db')
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM irrigation_data")
 print(cursor.fetchall())
